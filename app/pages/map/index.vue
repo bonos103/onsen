5b450c96f4f7be166051c11d60e810840180f004
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2020-03-18 23:10:21 +0900

    Test CSVから表示

--- app/pages/map/index.vue
+++ app/pages/map/index.vue
@@ -1,8 +1,21 @@
 <template lang="pug">
-  div hoge
+  div
+    div(v-for="item in items", :key="item.NAME")
+      div {{item.NAME}}
+      div {{item.URL}}
+      div {{item.ADDRESS}}
+      div {{item.PRICE}}
+      div {{item.type}}
 </template>
 <script>
+import csv from '@/assets/data/hokkaido.csv'
+
 export default {
   layout: 'map',
+  data() {
+    return {
+      items: csv,
+    }
+  },
 }
 </script>
