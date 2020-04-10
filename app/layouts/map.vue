3aee8e6ac83a05c08703bded7d45436f5bd83cc1
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2020-04-10 21:53:33 +0900

    ロゴ変更

--- app/layouts/map.vue
+++ app/layouts/map.vue
@@ -1,3 +1,25 @@
 <template lang="pug">
-  nuxt
+  div
+    h1(:class="$style.logo")
+      logo
+    nuxt
 </template>
+<script>
+import Logo from '@/components/Logo'
+
+export default {
+  components: {
+    Logo,
+  },
+}
+</script>
+<style lang="postcss" module>
+  .logo {
+    position: absolute;
+    left: 30px;
+    top: 15px;
+    width: 152px;
+    height: auto;
+    z-index: 9999;
+  }
+</style>
