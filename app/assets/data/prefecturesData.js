541611358cce2fb30ab69224d07be699d9ba3360
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2020-05-04 17:01:13 +0900

    fix

--- app/assets/data/prefecturesData.js
+++ app/assets/data/prefecturesData.js
@@ -1,50 +1,98 @@
-import hokkaido from '@/assets/data/hokkaido.yaml'
-import aomori from '@/assets/data/aomori.yaml'
-import akita from '@/assets/data/akita.yaml'
-import iwate from '@/assets/data/iwate.yaml'
-import yamagata from '@/assets/data/yamagata.yaml'
-import miyagi from '@/assets/data/miyagi.yaml'
-import fukushima from '@/assets/data/fukushima.yaml'
-import tokyo from '@/assets/data/tokyo.yaml'
-import kanagawa from '@/assets/data/kanagawa.yaml'
-import saitama from '@/assets/data/saitama.yaml'
-import chiba from '@/assets/data/chiba.yaml'
-import gunma from '@/assets/data/gunma.yaml'
-import ibaraki from '@/assets/data/ibaraki.yaml'
-import tochigi from '@/assets/data/tochigi.yaml'
-import niigata from '@/assets/data/niigata.yaml'
-import nagano from '@/assets/data/nagano.yaml'
-import yamanashi from '@/assets/data/yamanashi.yaml'
-import toyama from '@/assets/data/toyama.yaml'
-import ishikawa from '@/assets/data/ishikawa.yaml'
-import fukui from '@/assets/data/fukui.yaml'
-import aichi from '@/assets/data/aichi.yaml'
-import shizuoka from '@/assets/data/shizuoka.yaml'
-import gifu from '@/assets/data/gifu.yaml'
-import mie from '@/assets/data/mie.yaml'
-import osaka from '@/assets/data/osaka.yaml'
-import kyoto from '@/assets/data/kyoto.yaml'
-import hyogo from '@/assets/data/hyogo.yaml'
-import nara from '@/assets/data/nara.yaml'
-import shiga from '@/assets/data/shiga.yaml'
-import wakayama from '@/assets/data/wakayama.yaml'
-import tottori from '@/assets/data/tottori.yaml'
-import shimane from '@/assets/data/shimane.yaml'
-import okayama from '@/assets/data/okayama.yaml'
-import hiroshima from '@/assets/data/hiroshima.yaml'
-import yamaguchi from '@/assets/data/yamaguchi.yaml'
-import tokushima from '@/assets/data/tokushima.yaml'
-import kochi from '@/assets/data/kochi.yaml'
-import kagawa from '@/assets/data/kagawa.yaml'
-import ehime from '@/assets/data/ehime.yaml'
-import fukuoka from '@/assets/data/fukuoka.yaml'
-import saga from '@/assets/data/saga.yaml'
-import nagasaki from '@/assets/data/nagasaki.yaml'
-import kumamoto from '@/assets/data/kumamoto.yaml'
-import oita from '@/assets/data/oita.yaml'
-import miyazaki from '@/assets/data/miyazaki.yaml'
-import kagoshima from '@/assets/data/kagoshima.yaml'
-import okinawa from '@/assets/data/okinawa.yaml'
+import hokkaidoYaml from '@/assets/data/hokkaido.yaml'
+import aomoriYaml from '@/assets/data/aomori.yaml'
+import akitaYaml from '@/assets/data/akita.yaml'
+import iwateYaml from '@/assets/data/iwate.yaml'
+import yamagataYaml from '@/assets/data/yamagata.yaml'
+import miyagiYaml from '@/assets/data/miyagi.yaml'
+import fukushimaYaml from '@/assets/data/fukushima.yaml'
+import tokyoYaml from '@/assets/data/tokyo.yaml'
+import kanagawaYaml from '@/assets/data/kanagawa.yaml'
+import saitamaYaml from '@/assets/data/saitama.yaml'
+import chibaYaml from '@/assets/data/chiba.yaml'
+import gunmaYaml from '@/assets/data/gunma.yaml'
+import ibarakiYaml from '@/assets/data/ibaraki.yaml'
+import tochigiYaml from '@/assets/data/tochigi.yaml'
+import niigataYaml from '@/assets/data/niigata.yaml'
+import naganoYaml from '@/assets/data/nagano.yaml'
+import yamanashiYaml from '@/assets/data/yamanashi.yaml'
+import toyamaYaml from '@/assets/data/toyama.yaml'
+import ishikawaYaml from '@/assets/data/ishikawa.yaml'
+import fukuiYaml from '@/assets/data/fukui.yaml'
+import aichiYaml from '@/assets/data/aichi.yaml'
+import shizuokaYaml from '@/assets/data/shizuoka.yaml'
+import gifuYaml from '@/assets/data/gifu.yaml'
+import mieYaml from '@/assets/data/mie.yaml'
+import osakaYaml from '@/assets/data/osaka.yaml'
+import kyotoYaml from '@/assets/data/kyoto.yaml'
+import hyogoYaml from '@/assets/data/hyogo.yaml'
+import naraYaml from '@/assets/data/nara.yaml'
+import shigaYaml from '@/assets/data/shiga.yaml'
+import wakayamaYaml from '@/assets/data/wakayama.yaml'
+import tottoriYaml from '@/assets/data/tottori.yaml'
+import shimaneYaml from '@/assets/data/shimane.yaml'
+import okayamaYaml from '@/assets/data/okayama.yaml'
+import hiroshimaYaml from '@/assets/data/hiroshima.yaml'
+import yamaguchiYaml from '@/assets/data/yamaguchi.yaml'
+import tokushimaYaml from '@/assets/data/tokushima.yaml'
+import kochiYaml from '@/assets/data/kochi.yaml'
+import kagawaYaml from '@/assets/data/kagawa.yaml'
+import ehimeYaml from '@/assets/data/ehime.yaml'
+import fukuokaYaml from '@/assets/data/fukuoka.yaml'
+import sagaYaml from '@/assets/data/saga.yaml'
+import nagasakiYaml from '@/assets/data/nagasaki.yaml'
+import kumamotoYaml from '@/assets/data/kumamoto.yaml'
+import oitaYaml from '@/assets/data/oita.yaml'
+import miyazakiYaml from '@/assets/data/miyazaki.yaml'
+import kagoshimaYaml from '@/assets/data/kagoshima.yaml'
+import okinawaYaml from '@/assets/data/okinawa.yaml'
+
+export const hokkaido = hokkaidoYaml
+export const aomori = aomoriYaml
+export const akita = akitaYaml
+export const iwate = iwateYaml
+export const yamagata = yamagataYaml
+export const miyagi = miyagiYaml
+export const fukushima = fukushimaYaml
+export const tokyo = tokyoYaml
+export const kanagawa = kanagawaYaml
+export const saitama = saitamaYaml
+export const chiba = chibaYaml
+export const gunma = gunmaYaml
+export const ibaraki = ibarakiYaml
+export const tochigi = tochigiYaml
+export const niigata = niigataYaml
+export const nagano = naganoYaml
+export const yamanashi = yamanashiYaml
+export const toyama = toyamaYaml
+export const ishikawa = ishikawaYaml
+export const fukui = fukuiYaml
+export const aichi = aichiYaml
+export const shizuoka = shizuokaYaml
+export const gifu = gifuYaml
+export const mie = mieYaml
+export const osaka = osakaYaml
+export const kyoto = kyotoYaml
+export const hyogo = hyogoYaml
+export const nara = naraYaml
+export const shiga = shigaYaml
+export const wakayama = wakayamaYaml
+export const tottori = tottoriYaml
+export const shimane = shimaneYaml
+export const okayama = okayamaYaml
+export const hiroshima = hiroshimaYaml
+export const yamaguchi = yamaguchiYaml
+export const tokushima = tokushimaYaml
+export const kochi = kochiYaml
+export const kagawa = kagawaYaml
+export const ehime = ehimeYaml
+export const fukuoka = fukuokaYaml
+export const saga = sagaYaml
+export const nagasaki = nagasakiYaml
+export const kumamoto = kumamotoYaml
+export const oita = oitaYaml
+export const miyazaki = miyazakiYaml
+export const kagoshima = kagoshimaYaml
+export const okinawa = okinawaYaml
 
 export default {
   hokkaido,
