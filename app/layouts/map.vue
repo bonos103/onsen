01be28c942c45203cf6d0c43b2ccaeb9a3b0aac2
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2020-05-06 16:54:03 +0900

    ui

--- app/layouts/map.vue
+++ app/layouts/map.vue
@@ -25,13 +25,18 @@ export default {
 <style lang="postcss" module>
   .logo {
     position: absolute;
-    left: 30px;
+    left: 20px;
     top: 15px;
     width: 152px;
     height: auto;
     z-index: 100;
     @media (--md) {
+      left: 30px;
       width: 200px;
+    }
+    & svg {
+      height: auto;
+      padding: 10px 0;
     }
   }
 </style>
